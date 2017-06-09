d8b3e23bba733519dc0452048e296d1dcfbd9f60
somename <someemail>
2017-06-09 14:31:25 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -13,7 +13,15 @@ public class TestBowling {
 	@Test
 	public void testZaKonstruktor2() {
 		Frame f=new Frame(5,2);
-		assertEquals(5,f.getSecondThrow());
+		assertEquals(2,f.getSecondThrow());
 	}
+	
+	@Test
+	public void testZaScore() {
+		Frame f=new Frame(5,2);
+		int expected=5+2;
+		assertEquals(expected,f.score());
+	}
+	
 
 }
