7a72d3c95b7a6085c1a01df84073e9b4ac230d33
somename <someemail>
2017-06-09 14:45:27 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -15,7 +15,7 @@ public class BowlingGame {
 	// adds a frame to the game
 	public void addFrame(Frame frame) throws BowlingException{
 		if(frames.size()>10)
-			throw new BowlingException("Broj frejmova ne smije biti veci od 10!")
+			throw new BowlingException("Broj frejmova ne smije biti veci od 10!");
 	}
 	
 	// Sets the bonus throws at the end of the game
