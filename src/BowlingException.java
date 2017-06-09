f1b900eda3acbb3b7ee05417635ef60c6400dab1
somename <someemail>
2017-06-09 14:34:52 +0200

    besouro automatic message

--- src/BowlingException.java
+++ src/BowlingException.java
@@ -1,4 +1,14 @@
 
 public class BowlingException extends Exception {
 
+	public BowlingException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BowlingException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
 }
